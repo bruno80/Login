d073eec9285199faacb6da502101364dd1b4ccd0
Aluno <you@example.com>
2019-07-09 21:36:49 -0300

    part3

--- login/views.py
+++ login/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def home(request):
     return render (request, 'home.html')
+
+def list(request):
+    users = User.objects.all()
+    return render(request, 'user/list.html',{'users':users}) 
